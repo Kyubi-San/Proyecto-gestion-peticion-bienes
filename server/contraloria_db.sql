26ddf143c6fa1fa868bff712fe7152e1033b713b
Kyubi-San <javieragm27@outlook.es>
2025-03-12 18:14:06 -0400

    updated_db

--- server/contraloria_db.sql
+++ server/contraloria_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-03-2025 a las 16:14:39
+-- Tiempo de generación: 12-03-2025 a las 23:13:17
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -39,6 +39,13 @@ CREATE TABLE `bienes` (
   `comments` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `bienes`
+--
+
+INSERT INTO `bienes` (`id`, `name`, `type`, `description`, `withdrawalDate`, `approvalDate`, `requestDate`, `responsible`, `comments`) VALUES
+('68epPpDcAo', 'Pantalla', 'Electronico', 'LCD LG 24 pulgadas', '0000-00-00', '2025-03-11', '2025-03-11', 25, 'Urgente');
+
 -- --------------------------------------------------------
 
 --
@@ -53,6 +60,14 @@ CREATE TABLE `notificaciones` (
   `message` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `notificaciones`
+--
+
+INSERT INTO `notificaciones` (`id`, `sender`, `receiver`, `type`, `message`) VALUES
+(9, 24, 24, 1, 'Solicito un nuevo bien'),
+(10, 25, 25, 1, 'Solicito un nuevo bien');
+
 -- --------------------------------------------------------
 
 --
@@ -61,14 +76,22 @@ CREATE TABLE `notificaciones` (
 
 CREATE TABLE `pregunta_seguridad` (
   `id` int(11) NOT NULL,
-  `pregunta1` varchar(11) NOT NULL,
-  `pregunta2` varchar(11) NOT NULL,
-  `pregunta3` varchar(11) NOT NULL,
-  `respuesta1` varchar(11) NOT NULL,
-  `respuesta2` varchar(11) NOT NULL,
-  `respuesta3` varchar(11) NOT NULL,
+  `pregunta1` varchar(100) NOT NULL,
+  `pregunta2` varchar(100) NOT NULL,
+  `pregunta3` varchar(100) NOT NULL,
+  `respuesta1` varchar(100) NOT NULL,
+  `respuesta2` varchar(100) NOT NULL,
+  `respuesta3` varchar(100) NOT NULL,
   `id_usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `pregunta_seguridad`
+--
+
+INSERT INTO `pregunta_seguridad` (`id`, `pregunta1`, `pregunta2`, `pregunta3`, `respuesta1`, `respuesta2`, `respuesta3`, `id_usuario`) VALUES
+(12, '¿Cual es el apellido de tu abuelo?', '¿Cuál es su deporte favorito?', '¿Cómo se llamaba tu mamá?', 'Mata', 'Futbol', 'Roxalin', 25),
+(13, '¿Qué color le gusta más?', '¿Cuál es tu comida favorita?', '¿Cómo se llamaba tu mamá?', 'verde', 'pizza', 'roxalin', 26);
 
 -- --------------------------------------------------------
 
@@ -88,6 +111,13 @@ CREATE TABLE `solicitudes` (
   `aprobado` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `solicitudes`
+--
+
+INSERT INTO `solicitudes` (`n_solicitud`, `bien`, `descripcion`, `comentario`, `comentario_admin`, `tipo_bien`, `fecha_solicitud`, `id_usuario`, `aprobado`) VALUES
+(88, 'Pantalla', 'LCD LG 24 pulgadas', 'Urgente', '', 'Electronico', '2025-03-11', 25, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -105,6 +135,14 @@ CREATE TABLE `usuario` (
   `cedula` int(11) NOT NULL,
   `admin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`n_dependencia`, `nombre_dependencia`, `contrasena`, `username`, `nombre`, `apellido`, `correo`, `cedula`, `admin`) VALUES
+(25, 'Informatica', '$2y$10$OsDzTQkmSA9Wfh/JfApoM.9YyXN1wgMkcZypFouS4e1IQRojygWVm', 'Xander', 'Javier', 'Garcia', 'atam2005gm@gmail.com', 31533126, 1),
+(26, 'Contaduria', '$2y$10$eIQtdaRmRl0fXgySgB/YLOn3ef3Xg8JVNqUzcugnmNglOQlHEXiu6', 'User', 'Javier', 'Garcia', 'javieragm27@outlook.es', 31533126, 0);
 
 --
 -- Índices para tablas volcadas
@@ -148,25 +186,25 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `notificaciones`
 --
 ALTER TABLE `notificaciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `pregunta_seguridad`
 --
 ALTER TABLE `pregunta_seguridad`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `solicitudes`
 --
 ALTER TABLE `solicitudes`
-  MODIFY `n_solicitud` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
+  MODIFY `n_solicitud` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `n_dependencia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `n_dependencia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
